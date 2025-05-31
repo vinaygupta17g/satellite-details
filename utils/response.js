3228363353e921de87bb0957c5524459e998343f
vinay kumar <vk1230460@gmail.com>
2025-05-31 21:57:03 +0530

    update user

--- utils/response.js
+++ utils/response.js
@@ -1,11 +1,10 @@
 export let response=(res,data=null,message,success="ok",status=200)=>
+{
+    let responseData={
+        message,status,success}
+    if(data !=null)
     {
-        let responseData={
-            message,status,success
-        }
-        if(data !=null)
-        {
-            responseData.data=data;
-        }
-        return res.json(responseData)
+           responseData.data=data;
     }
+    return res.json(responseData)
+}
